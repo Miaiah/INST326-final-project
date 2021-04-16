b7044d28e9bc06924c537a4c371319d4e5fdcecb
Liyu Lo <loliyu0704@gmail.com>
2021-04-16 12:28:58 -0400

    added method to build a deck

--- trash_card_game.py
+++ trash_card_game.py
@@ -1,5 +1,5 @@
 """ Final project check in 1
-    
+
 """
 from enum import Enum
 
@@ -17,14 +17,14 @@ class Suit(Enum):
 
 class Card:
     """ A representation of a crad.
-    
+
     Attributes:
         suit(Suit): The suit of the Card object.
         number(int): The number of the Card object. 11 represents Jack.
                 12 represents Queen. 13 represents King.
-        revealed(bool): A boolean value represents if the card object 
+        revealed(bool): A boolean value represents if the card object
                 has been revealed.
-    
+
     """
     def __init__(self, suit, number, revealed):
         """ Initialize attributes for the card object.
@@ -33,84 +33,90 @@ class Card:
             suit (Suit): The suit of the Card object.
             number (int): The number of the Card object. 11 represents Jack.
                 12 represents Queen. 13 represents King.
-            revealed (bool): A boolean value represents if the card object 
+            revealed (bool): A boolean value represents if the card object
                 has been revealed.
-                
+
         Side effects:
-            Sets attributes suit, number, and revealed.   
-        
+            Sets attributes suit, number, and revealed.
         """
-        
+        self.suit = suit
+        self.number = number
+
     def reveal(self):
-        """ Set the revealed boolean to Ture to indicate the card has been 
+        """ Set the revealed boolean to Ture to indicate the card has been
             revealed.
-            
+
         """
-        
+
 class Deck:
     """ A deck of cards.
-    
+
     Attributes:
         cards(list): A list of cards.
-    
+
     """
-    
+
     def __init__(self):
         """ Generate a standard deck of 52 cards.
-        
+        """
+        self.cards = []
+        self.build_deck()
+
+    def build_deck():
+        """Build a deck
         Side effects:
             Set and modify self.cards.
-        
         """
-        
+        for suit in ["Spades", "Clubs", "Diamonds", "Hearts"]
+            for number in range(1,14):
+                self.cards.append(Card(suit, number))
+
     def shuffle():
         """ Randomly shuffle the list of cards.
-        
+
         Side effects:
             Modify self.cards
-        
+
         """
-        
+
     def deal():
-        """ Get the last card from the list of cards. 
-        
-        Return: 
+        """ Get the last card from the list of cards.
+
+        Return:
             Card: The last card from the list of cards.
-        
+
         Side effects:
             Modify self.cards
-        
+
         """
-    
-    
+
+
 class Player:
     """[summary]
     """
 
     def __init__(self):
-        
-        
+
+
 class GameStage(Enum):
     """A Enum class that represents the 4 type of game stages.
 
     Args:
         Enum (Enum): Enum class as the parent class.
-        
+
     """
     Player1Stage = 1
     Player2Stage = 2
     Player1Won = 3
     Player2Won = 4
-        
+
 
 class Game:
     """[summary]
     """
-    
+
     def __init__(self):
-        
-        
-        
+
+
+
 if __name__ == "__main__":
-        
-    
