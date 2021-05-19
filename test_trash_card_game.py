952a7395d15e7cd8d89e381456cae1df78ce8256
Mia Huang <hxmia467@gmail.com>
2021-05-19 00:53:06 -0700

    Add Pytest for deal() method in Deck class

---
+++ test_trash_card_game.py
@@ -0,0 +1,25 @@
+from pytest import fixture
+import trash_card_game as trash
+
+@fixture
+def deck1():
+    return trash.Deck()
+
+def test_deal(deck1):
+    """Does deal() deals the correct number of card and the right card?"""
+    """Deal 1 card"""
+    length = len(deck1.cards)
+    last_card = deck1.cards[-1]
+    dealed_card = deck1.deal()
+    assert last_card.number == dealed_card.number
+    assert last_card.suit == dealed_card.suit
+    assert dealed_card.revealed
+    assert len(deck1.cards) == length - 1
+    
+    """Deal 10 cards"""
+    length = len(deck1.cards)
+    dealed_cards = deck1.deal(10)
+    assert len(dealed_cards) == 10
+    assert len(deck1.cards) == length - 10
+    
+    
